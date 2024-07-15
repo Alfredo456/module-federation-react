ab77554c1be01921b62e5322e6ef3df02840424e
Alfredo <alfredo.canizales@globalr.net>
2024-07-15 13:26:33 -0300

    cambios

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 // @ts-ignore
-//import Auth from './components/Auth';
+import Auth from './components/Auth';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // @ts-ignore
-import Header from './remote/Header';
+//import Header from './remote/Header';
 
 const App = () => (
-    /*    <Router>
+        <Router>
             <Routes>
                 <Route path="/" element={<Auth/>}/>
             </Routes>
-        </Router>*/
-    <Header/>
+        </Router>
+/*    <Header/>*/
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
